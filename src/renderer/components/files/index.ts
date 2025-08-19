ac384a6b454358500fd4a3af7d9df25b28d484e8
Laurenz Bilek <e11904655@student.tuwien.ac.at>
2025-08-19 17:47:17 +0200

    refactor: add index files for common, files, layout, and ui components

---
+++ src/renderer/components/files/index.ts
@@ -0,0 +1,4 @@
+export { ApplicationFileUpload } from './ApplicationFileUpload';
+export { FileCard } from './FileCard';
+export { FileList } from './FileList';
+export { FileUpload } from './FileUpload';
